5fdc40a5bbfa94e9298d248910fc47863b271adf
shanelperera <shanelradperera@gmail.com>
2024-09-08 19:45:21 +0530

    adding interset.py

---
+++ Python/timer.py
@@ -0,0 +1,17 @@
+import time
+
+while True:
+    my_time = int(input("Enter the time in seconds: "))
+    if my_time <= 0:
+        print("Enter a valid time.")
+    else:
+        break
+        
+for i in range(my_time, 0, -1):
+    seconds = i % 60
+    minutes = int(i / 60) % 60
+    hours = int(i / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("TIME'S UP!")
